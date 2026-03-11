e68469419368df989c4d9e6aa2196bbee84b8c07
D Duy Anh Hoang <137117837+dduyanhhoang@users.noreply.github.com>
2026-03-11 09:41:25 +0000

    feat(author endpoint): Implement functionality to retrieve books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,25 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const results = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].author === author) {
+      results.push({
+        isbn: key,
+        title: books[key].title,
+        reviews: books[key].reviews
+      });
+    }
+  });
+
+  if (results.length > 0) {
+    res.send(JSON.stringify(results, null, 4));
+  } else {
+    res.status(404).json({message: "No books found by this author"});
+  }
 });
 
 // Get all books based on title
